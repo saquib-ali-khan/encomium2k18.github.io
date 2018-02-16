d60a5af5f6c1c08373f3f154ef8e8ef29ea8bec7
encomiumwebsite18 <36467262+encomiumwebsite18@users.noreply.github.com>
2018-02-16 20:10:21 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -98,32 +98,7 @@ var activities = [];
 var achievements = [];
 
 function loadPORS(){
-	// Make sure it is public or set to Anyone with link can view
-	var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/6/public/values?alt=json";
-	
-	var jqxhr = $.getJSON(url);
-	
-	// Set another completion function for the request above
-	jqxhr.done(function() {
-		entries = jqxhr.responseJSON.feed.entry;
-		
-		$(entries).each(function(){
-			var por ={};
-			por.post = this.gsx$post.$t;
-			por.organization = this.gsx$organization.$t;
-			por.description = this.gsx$description.$t;
-			por.duration = this.gsx$startdate.$t+"-"+this.gsx$enddate.$t;
-			pors.push(por);
-		});
-		
-		ko.applyBindings(pors, document.getElementById("pors"));
-		
-		$('.tile-header').matchHeight();
-		$('.tile-footer').matchHeight();
-		$('.tile').matchHeight();
-		$('.updateStatus').append('<p>Loaded: Positions of Responsibilities</p>');
-		count--;
-	});	
+	count--;
 }
 function loadWorkExp(){	
 	// Make sure it is public or set to Anyone with link can view
